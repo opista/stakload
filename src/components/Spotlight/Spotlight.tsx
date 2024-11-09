34f9b54884af733eedb38db448029a0d8c26e58b
metacurb <10343831+metacurb@users.noreply.github.com>
2024-11-09 00:47:38 +0000

    feat: start adding games to spotlight

--- src/components/Spotlight/Spotlight.tsx
+++ src/components/Spotlight/Spotlight.tsx
@@ -3,19 +3,43 @@ import {
   Spotlight as MantineSpotlight,
   SpotlightActionData,
 } from "@mantine/spotlight";
-import { IconSearch, IconSettings } from "@tabler/icons-react";
+import { IconHome, IconSearch } from "@tabler/icons-react";
 import { useTranslation } from "react-i18next";
+import { useRxData } from "rxdb-hooks";
+import { GameStoreModel } from "../../database/schema/game.schema";
 
 export const Spotlight = () => {
   const { t } = useTranslation();
+  const { result: games } = useRxData<GameStoreModel>("games", (collection) =>
+    collection.find()
+  );
 
-  const actions: SpotlightActionData[] = [];
+  const actions: SpotlightActionData[] = games.map(({ _id, name }) => ({
+    id: _id,
+    label: name,
+    description: t("lastPlayed", {
+      date: new Date(),
+      defaultValue: "Never",
+      formatParams: {
+        date: {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        },
+      },
+    }), // TODO
+    onClick: () => console.log("Play game"), // TODO
+    leftSection: (
+      <IconHome style={{ width: rem(24), height: rem(24) }} stroke={1.5} />
+    ),
+  }));
 
   return (
     <MantineSpotlight
       actions={actions}
+      limit={7}
       nothingFound={t("noResultsFound")}
-      highlightQuery
       searchProps={{
         leftSection: (
           <IconSearch
