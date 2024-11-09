6ca9295abdde548221e358c29d1cfa04dc5e0537
metacurb <10343831+metacurb@users.noreply.github.com>
2024-11-09 22:30:47 +0000

    chore: Make about info pull from package.json

--- src/desktop/components/Settings/AboutSettingsView/AboutSettingsView.tsx
+++ src/desktop/components/Settings/AboutSettingsView/AboutSettingsView.tsx
@@ -1,16 +1,20 @@
-import { ActionIcon } from "@mantine/core";
+import { ActionIcon, Code, Text } from "@mantine/core";
 import { os } from "@neutralinojs/lib";
 import { IconBrandGithub } from "@tabler/icons-react";
+import { homepage, version } from "../../../../../package.json";
 
 export const AboutSettingsView = () => {
   return (
     <div>
+      <Text>
+        Trulaunch <Code>v{version}</Code>
+      </Text>
       <ActionIcon
         aria-label="Github repository"
         color="gray"
         size="xl"
         variant="white"
-        onClick={() => os.open("https://github.com/opista/trulaunch")}
+        onClick={() => os.open(homepage)}
       >
         <IconBrandGithub />
       </ActionIcon>
