ee3a1f33d4f2363bbf59f0a31e48ef48a48b4a1d
metacurb <10343831+metacurb@users.noreply.github.com>
2024-11-28 20:17:45 +0000

    chore: tighten up settings select

--- src/renderer/src/components/Settings/SettingsSelect/SettingsSelect.tsx
+++ src/renderer/src/components/Settings/SettingsSelect/SettingsSelect.tsx
@@ -1,9 +1,11 @@
 import { Select, SelectProps } from "@mantine/core";
 import classes from "./SettingsSelect.module.css";
 
-type SettingsSelectProps = SelectProps & {};
+type SettingsSelectProps = SelectProps & {
+  onChange: (value: string) => void;
+};
 
-export const SettingsSelect = (props: SettingsSelectProps) => {
+export const SettingsSelect = ({ onChange, ...props }: SettingsSelectProps) => {
   return (
     <Select
       allowDeselect={false}
@@ -12,6 +14,7 @@ export const SettingsSelect = (props: SettingsSelectProps) => {
         root: classes.root,
         input: classes.input,
       }}
+      onChange={(value) => onChange?.(value as string)}
       {...props}
       inputSize="xs"
       withCheckIcon={false}
