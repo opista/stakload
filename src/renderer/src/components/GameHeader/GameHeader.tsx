37d28a901bf53242324966709a2d8d352d8464e5
BeauAgst <10343831+metacurb@users.noreply.github.com>
2024-12-09 16:41:50 +0000

    chore: move game header into own component

---
+++ src/renderer/src/components/GameHeader/GameHeader.tsx
@@ -0,0 +1,49 @@
+import { ActionIcon } from "@components/ActionIcon/ActionIcon";
+import { RemoveGameModal } from "@components/RemoveGameModal/RemoveGameModal";
+import { GameStoreModel } from "@contracts/database/games";
+import { Container, Flex, Group } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconArrowLeft, IconPencil, IconTrash } from "@tabler/icons-react";
+import { t } from "i18next";
+import { useNavigate } from "react-router";
+
+import classes from "./GameHeader.module.css";
+
+type GameHeaderProps = {
+  game: GameStoreModel;
+};
+
+export const GameHeader = ({ game }: GameHeaderProps) => {
+  const [openedDelete, { open: openDelete, close: closeDelete }] = useDisclosure(false);
+  const navigate = useNavigate();
+
+  const navigateToGamesList = () => navigate("..", { relative: "path" });
+
+  const onRemoveConfirm = async (preventReadd: boolean) => {
+    if (!game) return;
+    await window.api.removeGame(game._id, preventReadd);
+    navigate("..");
+    closeDelete();
+  };
+
+  return (
+    <Container size="responsive">
+      <Flex className={classes.header} justify="space-between">
+        <Group>
+          <ActionIcon aria-label={t("goBack")} icon={IconArrowLeft} onClick={navigateToGamesList} />
+        </Group>
+        <RemoveGameModal
+          gameTitle={game.name}
+          onConfirm={onRemoveConfirm}
+          onClose={closeDelete}
+          opened={openedDelete}
+        />
+        <Group gap="xs">
+          {/* TODO - Do we even want to implement this yet? */}
+          <ActionIcon aria-label={t("edit")} disabled icon={IconPencil} onClick={() => console.log("edit")} />
+          <ActionIcon aria-label={t("delete")} icon={IconTrash} onClick={openDelete} />
+        </Group>
+      </Flex>
+    </Container>
+  );
+};
