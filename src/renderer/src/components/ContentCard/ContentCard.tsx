ef27b9994dac77f5eca9df587e74e4f2f857c922
BeauAgst <10343831+metacurb@users.noreply.github.com>
2025-01-09 14:46:48 +0000

    chore: content card tweaks

--- src/renderer/src/components/ContentCard/ContentCard.tsx
+++ src/renderer/src/components/ContentCard/ContentCard.tsx
@@ -5,11 +5,12 @@ import classes from "./ContentCard.module.css";
 
 type ContentCardProps = {
   children: ReactNode;
+  className?: string;
   title?: ReactNode;
 };
 
-export const ContentCard = ({ children, title }: ContentCardProps) => (
-  <Paper classNames={{ root: classes.container }}>
+export const ContentCard = ({ children, className, title }: ContentCardProps) => (
+  <Paper className={className} classNames={{ root: classes.container }} radius="lg">
     {title && (
       <Title classNames={{ root: classes.title }} order={2}>
         {title}
