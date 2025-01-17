f34c138f16e83e1681882af7301870d6c3ea8a31
BeauAgst <10343831+metacurb@users.noreply.github.com>
2025-01-17 23:07:57 +0000

    chore: absorb games carousel children

--- src/renderer/src/components/GamesCarousel/GamesCarousel.tsx
+++ src/renderer/src/components/GamesCarousel/GamesCarousel.tsx
@@ -1,18 +1,31 @@
+import { GameCover } from "@components/GameCover/GameCover";
+import { GameStoreModel } from "@contracts/database/games";
 import { Carousel } from "@mantine/carousel";
 import { Stack, Title } from "@mantine/core";
+import { useViewportSize } from "@mantine/hooks";
 import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
-import { ReactNode } from "react";
+import { useMemo } from "react";
+import { useNavigate } from "react-router";
 
 import classes from "./GamesCarousel.module.css";
 
 type GamesCarouselProps = {
-  children: ReactNode;
+  games: GameStoreModel[];
   loop?: boolean;
-  slideCount: number;
   title: string;
 };
 
-export const GamesCarousel = ({ children, loop, slideCount, title }: GamesCarouselProps) => {
+export const GamesCarousel = ({ games, loop, title }: GamesCarouselProps) => {
+  const navigate = useNavigate();
+  const { width } = useViewportSize();
+
+  const slideCount = useMemo(() => {
+    if (width >= 1900) return 7;
+    if (width >= 1700) return 6;
+    if (width >= 1500) return 5;
+    return 4;
+  }, [width]);
+
   return (
     <Stack>
       <Title order={2}>{title}</Title>
@@ -31,7 +44,11 @@ export const GamesCarousel = ({ children, loop, slideCount, title }: GamesCarous
         slideSize={`${90 / slideCount}%`}
         slidesToScroll={slideCount}
       >
-        {children}
+        {games.map((game) => (
+          <Carousel.Slide key={game._id}>
+            <GameCover game={game} hoverEffect={false} onClick={(game) => navigate(`/desktop/games/${game._id}`)} />
+          </Carousel.Slide>
+        ))}
       </Carousel>
     </Stack>
   );
