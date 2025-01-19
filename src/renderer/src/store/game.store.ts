0f77ec36a73ecf73183cf999d53e9bef9992d6fb
BeauAgst <10343831+metacurb@users.noreply.github.com>
2025-01-19 20:58:16 +0000

    chore: stop persisting game state

--- src/renderer/src/store/game.store.ts
+++ src/renderer/src/store/game.store.ts
@@ -1,80 +1,75 @@
 import { GameStoreModel } from "@contracts/database/games";
 import { GameActions, GameState } from "@contracts/store/game";
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
 const DEFAULT_FILTERS = {
+  ageRatings: undefined,
   developers: undefined,
   gameModes: undefined,
   genres: undefined,
+  libraries: undefined,
+  platforms: undefined,
   playerPerspectives: undefined,
   publishers: undefined,
 };
 
 type GameStore = GameState & GameActions;
 
-export const useGameStore = create<GameStore>()(
-  persist(
-    (set, get) => ({
-      gamesList: [],
-      newGames: [],
-      gamesDetails: {},
-      gamesPreview: {},
+export const useGameStore = create<GameStore>()((set, get) => ({
+  gamesList: [],
+  newGames: [],
+  gamesDetails: {},
+  gamesPreview: {},
 
-      fetchGamesList: async () => {
-        const games = await window.api.getGamesList();
-        set({ gamesList: games });
-      },
+  fetchGamesList: async () => {
+    const games = await window.api.getGamesList();
+    set({ gamesList: games });
+  },
 
-      fetchNewGames: async () => {
-        const games = await window.api.getNewGames();
-        set({ newGames: games });
-      },
+  fetchNewGames: async () => {
+    const games = await window.api.getNewGames();
+    set({ newGames: games });
+  },
 
-      fetchGameDetails: async (id: string) => {
-        const details = await window.api.getGameById(id);
-        set((state) => ({
-          gamesDetails: {
-            ...state.gamesDetails,
-            [id]: details,
-          },
-        }));
+  fetchGameDetails: async (id: string) => {
+    const details = await window.api.getGameById(id);
+    set((state) => ({
+      gamesDetails: {
+        ...state.gamesDetails,
+        [id]: details,
       },
+    }));
+  },
 
-      currentGame: undefined,
-      collections: [],
-      fetchCollections: async () => {
-        const collections = await window.api.getCollections();
-        set({ collections });
+  currentGame: undefined,
+  collections: [],
+  fetchCollections: async () => {
+    const collections = await window.api.getCollections();
+    set({ collections });
+  },
+  setCurrentCollection: (id: string) => {
+    const currentCollection = get().collections.find(({ _id }) => _id === id);
+    set({ currentCollection });
+  },
+  selectedCollection: "",
+  selectedGame: null,
+  selectedFilters: DEFAULT_FILTERS,
+  resetFilters: () => set({ selectedFilters: DEFAULT_FILTERS }),
+  setMultipleFilters: (filters: Partial<GameState["selectedFilters"]>) =>
+    set(() => ({
+      selectedFilters: {
+        ...DEFAULT_FILTERS,
+        ...filters,
       },
-      setCurrentCollection: (id: string) => {
-        const currentCollection = get().collections.find(({ _id }) => _id === id);
-        set({ currentCollection });
+    })),
+  setCurrentGame: (game: GameStoreModel) => set({ currentGame: game }),
+  setSelectedCollection: (selectedCollection: string) => set({ selectedCollection }),
+  setSelectedGame: (selectedGame: string | null) => set({ selectedGame }),
+  setSelectedFilter: (key: keyof GameState["selectedFilters"], value: string[]) =>
+    set((state) => ({
+      selectedFilters: {
+        ...state.selectedFilters,
+        [key]: value,
       },
-      selectedCollection: "",
-      selectedGame: null,
-      selectedFilters: DEFAULT_FILTERS,
-      resetFilters: () => set({ selectedFilters: DEFAULT_FILTERS }),
-      setMultipleFilters: (filters: Partial<GameState["selectedFilters"]>) =>
-        set(() => ({
-          selectedFilters: {
-            ...DEFAULT_FILTERS,
-            ...filters,
-          },
-        })),
-      setCurrentGame: (game: GameStoreModel) => set({ currentGame: game }),
-      setSelectedCollection: (selectedCollection: string) => set({ selectedCollection }),
-      setSelectedGame: (selectedGame: string | null) => set({ selectedGame }),
-      setSelectedFilter: (key: keyof GameState["selectedFilters"], value: string[]) =>
-        set((state) => ({
-          selectedFilters: {
-            ...state.selectedFilters,
-            [key]: value,
-          },
-        })),
-    }),
-    {
-      name: "game",
-    },
-  ),
-);
+    })),
+}));
