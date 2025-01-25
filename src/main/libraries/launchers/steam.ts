26c86c0e95564f6219567718be5548860a9c8162
metacurb <10343831+metacurb@users.noreply.github.com>
2025-01-25 22:49:30 +0000

    chore: TODO on launchers

--- src/main/libraries/launchers/steam.ts
+++ src/main/libraries/launchers/steam.ts
@@ -3,6 +3,9 @@ import { shell } from "electron";
 
 const STEAM_LAUNCHER_BASE_URL = "steam://";
 
+// TODO - What happens if the command fails? We should look at handling
+// this and returning an error in the UI about the launcher not being
+// installed
 export const launchSteamGame = async (game: GameStoreModel) =>
   shell.openExternal(`${STEAM_LAUNCHER_BASE_URL}run/${game.gameId}`);
 
