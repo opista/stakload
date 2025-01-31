c8decda1fcee5c22cb5838ce201a6834bd62340a
metacurb <10343831+metacurb@users.noreply.github.com>
2025-01-31 00:50:26 +0000

    chore: simple logic for action buttons

--- src/renderer/src/components/Desktop/GameControls/GameControls.tsx
+++ src/renderer/src/components/Desktop/GameControls/GameControls.tsx
@@ -4,9 +4,15 @@ import { Button, Group } from "@mantine/core";
 export const GameControls = ({ game }: { game: GameStoreModel }) => {
   return (
     <Group>
-      <Button onClick={() => window.api.launchGame(game._id)}>Launch</Button>
-      <Button onClick={() => window.api.uninstallGame(game._id)}>Install</Button>
-      <Button onClick={() => window.api.uninstallGame(game._id)}>Uninstall</Button>
+      <Button disabled={!game.isInstalled} onClick={() => window.api.launchGame(game._id)}>
+        Launch
+      </Button>
+      <Button disabled={game.isInstalled} onClick={() => window.api.installGame(game._id)}>
+        Install
+      </Button>
+      <Button disabled={!game.isInstalled} onClick={() => window.api.uninstallGame(game._id)}>
+        Uninstall
+      </Button>
     </Group>
   );
 };
