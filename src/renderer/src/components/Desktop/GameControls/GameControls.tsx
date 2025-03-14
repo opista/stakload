fee3274024a7cb2081da206a183287ce3a30f3b1
BeauAgst <10343831+metacurb@users.noreply.github.com>
2025-03-14 16:59:24 +0000

    chore: Display correct controls on details page

--- src/renderer/src/components/Desktop/GameControls/GameControls.tsx
+++ src/renderer/src/components/Desktop/GameControls/GameControls.tsx
@@ -1,18 +1,34 @@
 import { GameStoreModel } from "@contracts/database/games";
 import { Button, Group } from "@mantine/core";
+import { IconDownload, IconPlayerPlayFilled } from "@tabler/icons-react";
 
 export const GameControls = ({ game }: { game: GameStoreModel }) => {
   return (
     <Group>
-      <Button disabled={!game.isInstalled} onClick={() => window.api.launchGame(game._id)}>
-        Launch
-      </Button>
-      <Button disabled={game.isInstalled} onClick={() => window.api.installGame(game._id)}>
-        Install
-      </Button>
-      <Button disabled={!game.isInstalled} onClick={() => window.api.uninstallGame(game._id)}>
-        Uninstall
-      </Button>
+      {game.isInstalled && (
+        <>
+          <Button
+            color="green"
+            disabled={!game.isInstalled}
+            leftSection={<IconPlayerPlayFilled size={16} />}
+            onClick={() => window.api.launchGame(game._id)}
+          >
+            Launch
+          </Button>
+          <Button color="red" disabled={!game.isInstalled} onClick={() => window.api.uninstallGame(game._id)}>
+            Uninstall
+          </Button>
+        </>
+      )}
+      {!game.isInstalled && (
+        <Button
+          disabled={game.isInstalled}
+          leftSection={<IconDownload size={16} />}
+          onClick={() => window.api.installGame(game._id)}
+        >
+          Install
+        </Button>
+      )}
     </Group>
   );
 };
