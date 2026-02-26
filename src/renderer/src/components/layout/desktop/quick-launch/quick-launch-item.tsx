33accd5938c7cef8180a13168d243535d6ef3c13
BeauAgst <10343831+metacurb@users.noreply.github.com>
2026-02-26 17:00:56 +0000

    add nice transition to quick launch item

--- src/renderer/src/components/layout/desktop/quick-launch/quick-launch-item.tsx
+++ src/renderer/src/components/layout/desktop/quick-launch/quick-launch-item.tsx
@@ -1,7 +1,7 @@
 ﻿import { GameCover } from "@components/game/game-cover";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import { IconPlayerPlayFilled } from "@tabler/icons-react";
+import { IconDownload, IconPlayerPlay } from "@tabler/icons-react";
 import { cn } from "@util/cn";
 import { useTranslation } from "react-i18next";
 
@@ -37,18 +37,18 @@ export const QuickLaunchItem = ({ editMode, game }: QuickLaunchItemProps) => {
     <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
       <div
         className={cn(
-          "group flex cursor-pointer items-center gap-2 rounded-lg p-2 pr-3 transition-colors text-slate-400",
-          !editMode && "hover:bg-primary/10 hover:text-primary",
+          "group relative z-50 flex w-max min-w-full max-w-full cursor-pointer items-center gap-2 rounded-lg p-2 pr-3 transition-all duration-300 ease-in-out text-slate-400 hover:max-w-[200%] hover:pr-5 hover:shadow-gold-hover",
+          !editMode && "hover:bg-stone-950 hover:text-primary hover:ring-1 hover:ring-white/10",
         )}
         onClick={onClick}
       >
         <GameCover className="w-9 shrink-0 rounded-md" game={game} showGameTitle={false} />
         <div className="relative flex w-full min-w-0 flex-col text-xs font-bold uppercase">
-          <span className="block w-full truncate tracking-wider transition-transform duration-300 ease-in-out group-hover:-translate-y-1/2">
+          <span className="truncate tracking-wider transition-all duration-300 ease-in-out group-hover:-translate-y-1/2">
             {game.name}
           </span>
           <div className="absolute bottom-0 flex translate-y-full items-center gap-1 opacity-0 transition-all duration-300 ease-in-out group-hover:translate-y-1/2 group-hover:opacity-100">
-            <IconPlayerPlayFilled size={10} />
+            {game.isInstalled ? <IconPlayerPlay size={12} /> : <IconDownload size={12} />}
             <span>{game.isInstalled ? t("quickLaunch.launch") : t("quickLaunch.install")}</span>
           </div>
         </div>
